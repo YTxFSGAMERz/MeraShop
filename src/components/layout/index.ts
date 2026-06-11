fadfcb26a8a2de74478e2bd1e1ab9a3ab1883651
Farhan <94067297+YTxFSGAMERz@users.noreply.github.com>
2026-06-11 08:38:25 +0530

    Add src/components/layout/index.ts

---
+++ src/components/layout/index.ts
@@ -0,0 +1,9 @@
+// Layout component barrel exports
+export { SiteHeader } from './SiteHeader';
+export { SiteFooter } from './SiteFooter';
+export { MobileBottomNav } from './MobileBottomNav';
+export { MobileMenu } from './MobileMenu';
+export { SearchBar } from './SearchBar';
+export { BreadcrumbNav } from './Breadcrumb';
+export type { BreadcrumbItemData } from './Breadcrumb';
+export { NotificationCenter } from './NotificationCenter';
