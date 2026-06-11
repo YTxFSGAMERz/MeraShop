ca1ed7e1026b38acc4a3c256c21a503d47c8660b
Farhan <94067297+YTxFSGAMERz@users.noreply.github.com>
2026-06-11 08:38:14 +0530

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  allowedDevOrigins: ["*.space-z.ai"],
+  experimental: {
+    turbo: {
+      memoryLimit: 2048,
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
